1ae30a8bc22f1f246d3be0a1d904966c18b89dfd
udara manupriya <udara.manupriya@sysco.com>
2023-10-19 23:26:49 +0530

    adding lambda Test mobile

--- src/test/java/com/lambdatest/sampleTest/MobileTest.java
+++ src/test/java/com/lambdatest/sampleTest/MobileTest.java
@@ -1,42 +1,48 @@
 package com.lambdatest.sampleTest;
 
 import com.common.Constants;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
-import java.util.Map;
 
-public class WebTest {
-    RemoteWebDriver driver;
-    @Test
+public class MobileTest {
+    AppiumDriver driver;
+    @BeforeClass
     public void connectToLambdaTest() throws MalformedURLException {
-        ChromeOptions browserOptions = new ChromeOptions();
-        browserOptions.setPlatformName("Windows 11");
-        browserOptions.setBrowserVersion("104.0");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
         HashMap<String, Object> ltOptions = new HashMap<String, Object>();
-        ltOptions.put("selenium_version", "4.0.0");
         ltOptions.put("w3c", true);
-        browserOptions.setCapability("LT:Options", ltOptions);
-        driver = new RemoteWebDriver(new URL("https://udara.manupriya:Ejvwi0FShhVeYpQW2fUwemw88y2DzBbiwFCWdQqYXck9T8WUcM@hub.lambdatest.com/wd/hub"), browserOptions);
+        ltOptions.put("platformName", "android");
+        ltOptions.put("deviceName", "Pixel 6 Pro");
+        ltOptions.put("platformVersion", "13");
+        capabilities.setCapability("lt:options", ltOptions);
+
+        driver = new AndroidDriver(new URL("https://udara.manupriya:Ejvwi0FShhVeYpQW2fUwemw88y2DzBbiwFCWdQqYXck9T8WUcM@hub.lambdatest.com/wd/hub"), capabilities);
+
     }
+
     @Test
-    public void testSourceDemo(){
+    public void TestSourceDemoGooglePixelPro(){
         driver.get("https://www.saucedemo.com");
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
-        Assert.assertEquals(driver.getTitle(),"Swag Labs");
+        Assert.assertEquals(driver.getTitle(), "Swag Labs");
     }
 
     @AfterClass
     public void close(){
         driver.quit();
     }
+
 }
